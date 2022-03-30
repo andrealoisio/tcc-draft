d502e614aec9e9025dc35320fcb6c25ff5665658
Daniel Mariano Castilho <daniel.castilho@serpro.gov.br>
2022-03-30 17:59:22 -0300

    Refatoração de Header

--- apps/gisa-ui/src/components/Layout/Header.js
+++ apps/gisa-ui/src/components/Layout/Header.js
@@ -1,12 +1,18 @@
-import './header.css'
 import React from 'react'; 
 import { Box } from '@material-ui/core';
+import { Tooltip } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
     return(
-        // <Link
-        // <Box
-        <a href="/"><img src="gisa.png" alt="GisaLogo" className="logo"/></a>
+        <Link to={"/"} >
+            <Tooltip title="Home">
+                <Box
+                    component="img"
+                    alt="Home"
+                    src="gisa.png"
+                />
+            </Tooltip>
+        </Link >
     )
 }
