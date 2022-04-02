99a19f4638fe14bca5cfd2d317c8b806cb9d0b8f
Daniel Mariano Castilho <daniel.castilho@serpro.gov.br>
2022-04-02 14:10:36 -0300

    remover import

--- apps/gisa-ui/src/components/DataTable.js
+++ apps/gisa-ui/src/components/DataTable.js
@@ -1,6 +1,5 @@
 import { DataGrid } from '@mui/x-data-grid';
 import React, { useState, useEffect } from 'react';
-import { makeStyles } from "@material-ui/core/styles";
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
