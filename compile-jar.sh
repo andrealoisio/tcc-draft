239f192e3e0f7fd5c922b2d26b7238b891261f35
André Aloisio Mendes <andrealoisio@gmail.com>
2022-04-03 12:28:07 -0300

    chore: include data generator on build

--- compile-jar.sh
+++ compile-jar.sh
@@ -3,3 +3,11 @@ cd apps/gisa-informacoes-cadastrais
 docker run -it --rm -v "$HOME/.m2":/root/.m2 -v "$(pwd)":/usr/src/myapp -w /usr/src/myapp openjdk:11 ./mvnw clean package -Dmaven.test.skip=true
 cd ..
 cd ..
+cd apps/gerador-dados-legado
+docker run -it --rm -v "$HOME/.m2":/root/.m2 -v "$(pwd)":/usr/src/myapp -w /usr/src/myapp openjdk:11 ./mvnw clean package -Dmaven.test.skip=true
+cd ..
+cd ..
+cd apps/gisa-ui
+npm run build
+cd ..
+cd ..
